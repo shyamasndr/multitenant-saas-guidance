082223bd09c1bd1bf499d75c007c688e49ea4053
Mani <manikris@microsoft.com>
2015-12-14 15:46:44 -0800

    added missing files

---
+++ src/Tailspin.Surveys.TokenStorage.Redis/RedisTokenCacheServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Tailspin.Surveys.TokenStorage.Redis;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class TokenStorageServiceCollectionExtensions
+    {
+        public static void AddRedisTokenStorage(this IServiceCollection services)
+        {
+            // Add the default token service
+            services.AddScoped<ITokenCacheService, RedisTokenCacheService>();
+        }
+    }
+}
