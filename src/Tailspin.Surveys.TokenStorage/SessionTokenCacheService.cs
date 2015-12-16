ee58c87216b0236f1e3d5d6deb5de654b90fce7d
Mani <manikris@microsoft.com>
2015-12-16 13:16:56 -0800

    fixed bug where token cache was getting created when user was not signed in yet

--- src/Tailspin.Surveys.TokenStorage/SessionTokenCacheService.cs
+++ src/Tailspin.Surveys.TokenStorage/SessionTokenCacheService.cs
@@ -6,11 +6,14 @@ using Microsoft.AspNet.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Tailspin.Surveys.Common;
+using System.Security.Claims;
 
 namespace Tailspin.Surveys.TokenStorage
 {
     public class SessionTokenCacheService : TokenCacheService
     {
+        private IHttpContextAccessor _contextAccessor;
+
         /// <summary>
         /// Initializes a new instance of <see cref="Tailspin.Surveys.TokenStorage.SessionTokenCacheService"/>
         /// </summary>
@@ -21,7 +24,7 @@ namespace Tailspin.Surveys.TokenStorage
         {
             Guard.ArgumentNotNull(contextAccessor, nameof(contextAccessor));
             Guard.ArgumentNotNull(loggerFactory, nameof(loggerFactory));
-            _cache = new SessionTokenCache(contextAccessor, _loggerFactory);
+            _contextAccessor = contextAccessor;
         }
 
         /// <summary>
@@ -32,6 +35,14 @@ namespace Tailspin.Surveys.TokenStorage
         /// <returns>An instance of <see cref="Microsoft.IdentityModel.Clients.ActiveDirectory.TokenCache"/>.</returns>
         public override Task<TokenCache> GetCacheAsync(string userObjectId, string clientId)
         {
+            if (!_contextAccessor.HttpContext.User.IsSignedInToApplication())
+            {
+                throw new InvalidOperationException("User needs to be authenticated first");
+            }
+            if (_cache == null)
+            {
+                _cache = new SessionTokenCache(_contextAccessor, _loggerFactory);
+            }
             return Task.FromResult(_cache);
         }
     }
